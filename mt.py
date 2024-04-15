f3f2d9974faa0d19a03fef0732be9bfb326dd838
Rana Zakwan <167029020+Rana-Zakwan@users.noreply.github.com>
2024-04-15 12:19:31 +0500

    Update mt.py

--- mt.py
+++ mt.py
@@ -1317,7 +1317,7 @@ def Subscraption():
 	key1=open('/data/data/com.termux/files/usr/bin/.mrBALOCH -cov', 'r').read()
 	clear()
 	print(logo)
-	r1=requests.get("https://github.com/Rana-Zakwan/File/blob/main/File.txthttps://github.com/Rana-Zakwan/File/blob/main/File.txt").text
+	r1=requests.get("https://github.com/Rana-Zakwan/trick/blob/main/trick.txt").text
 	if key1 in r1:
 		os.system('clear')
 		print(logo)
