856442096cf739181a6224d05e5b90895af5a341
Rana Zakwan <167029020+Rana-Zakwan@users.noreply.github.com>
2024-04-15 12:57:37 +0500

    Update mt.py

--- mt.py
+++ mt.py
@@ -1314,41 +1314,54 @@ class Main:
 
 
 def Subscraption():
-	key1=open('/data/data/com.termux/files/usr/bin/.mrBALOCH -cov', 'r').read()
-	clear()
-	print(logo)
-	r1=requests.get("https://github.com/Rana-Zakwan/trick/blob/main/trick.txt").text
-	if key1 in r1:
-		os.system('clear')
-		print(logo)
-		Main()
-	else:
-		os.system("clear")
-		print(logo)
-		print("\t \033[1;32m First Get Approvel\033[1;37m ")
-		time.sleep(1)
-		os.system("clear")
-		print(logo)
-		print ("")
-		print(" \033[1;32m ALIYAN Toll Free BUT You Need Get Approved First\033[1;37m\n")
-		print(" \033[1;32m Note : ALIYAN FREE HA BHAIYO ENJOYA   \033[1;37m")
-		print ("")
-		print(" Your Key is Not Approved ")
-		print("")
-		print(" Copy And Send Key To Admin")
-		print ("")
-		print (" Your Key : "+ak+ah+key1 )
-		print ("")
-		name = input(" Your Name : ")
-		print ("")
-		gf = input(" Your gf Name : ")
-		print ("")
-		lol = input(" Your Your Email : ")
-		print ("")
-		input(" Press Enter To Send Key")
-		time.sleep(3.5)
-		tks = 'Dear%20Admin,%20Please%20Approved%20My%20Key%20To%20Premium%20%20Thanks%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20Email%20:%20'+lol+'%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20Name%20:%20'+name+'%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20%20Key%20%20:%20'+ak+ah+key1
-		os.system('am start https://wa.me/+913216851651?text=' + tks)
-		Subscraption()        
-Subscraption()
+    # Link to the text file containing approval keys
+    link_to_text_file = "https://raw.githubusercontent.com/Rana-Zakwan/trick/main/trick.txt"
+    
+    # Asking the user to input the approval key
+    key1 = input("Enter your approval key: ")
+    
+    # Clearing the screen
+    os.system('clear')
+    
+    # Assuming 'logo' is defined elsewhere in your code
+    print(logo)
+    
+    # Fetching the approval keys from the provided link
+    r1 = requests.get(link_to_text_file).text
+    
+    # Checking if the key is present in the fetched data
+    if key1 in r1:
+        os.system('clear')
+        print(logo)
+        Main()
+    else:
+        os.system("clear")
+        print(logo)
+        print("\t \033[1;32m First Get Approval\033[1;37m ")
+        time.sleep(1)
+        os.system("clear")
+        print(logo)
+        print ("")
+        print(" \033[1;32m ALIYAN Toll Free BUT You Need to Get Approved First\033[1;37m\n")
+        print(" \033[1;32m Note : ALIYAN IS FREE BHAIYO ENJOY \033[1;37m")
+        print ("")
+        print(" Your Key is Not Approved ")
+        print("")
+        print(" Copy And Send Key To Admin")
+        print ("")
+        print (" Your Key : " + key1 )
+        print ("")
+        name = input(" Your Name : ")
+        print ("")
+        gf = input(" Your gf Name : ")
+        print ("")
+        lol = input(" Your Your Email : ")
+        print ("")
+        input(" Press Enter To Send Key")
+        time.sleep(3.5)
+        tks = 'Dear%20Admin,%20Please%20Approve%20My%20Key%20For%20Premium%20Membership.%20Thanks.%20My%20Email:%20' + lol + '%20My%20Name:%20' + name + '%20My%20Key:%20' + key1
+        os.system('am start https://wa.me/+923216851651?text=' + tks)
+        Subscraption()        
 
+# Calling the Subscraption function to start the process
+Subscraption()
